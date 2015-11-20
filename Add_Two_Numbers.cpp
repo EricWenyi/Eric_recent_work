94643659e9bd6b705c3e4b8fb47b8e7ad5e5b753
Eric Wenyi <ericwenyi1995@outlook.com>
2015-11-20 20:31:48 +0800

    This is cpp file for the Leetcode prolem Add_Two_Numbers
    During this file I made a lot of mistakes. To see the detail please see
    the file's annotation/

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,85 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *results=new ListNode(0);
+        ListNode *p1,*p2;
+        ListNode *N1, *N2;
+        int j = 0;
+
+        //results = new ListNode;
+        p1 = results;
+        p2 = results;
+
+        N1 = l1;
+        N2 = l2;
+        
+        do
+        {
+        	
+        		p1->val += N1->val + N2->val;
+        	
+        	//cout<<p1->val<<" "<<endl;
+
+        	if (p1->val >= 10){
+        		p1->val = (p1->val)%10;
+        		j++;
+        	}
+        	
+        	if(j == 0){
+        	    
+        		if ((N1->next)==NULL && (N2->next)==NULL){
+        		    
+        			return results;
+        		}
+        		else{
+        			p1->next = new ListNode(0);
+        		    p1 = p1->next;
+        		}
+        	}
+
+        	if (j == 1){
+        		p1->next = new ListNode(1);
+        		p1 = p1->next;
+        		j--;
+        	}
+        	
+        	
+        	//cout<<p1->val<<" "<<endl;
+        	
+        	if((N1->next) == NULL && (N2->next) == NULL){
+        		return results;
+        	}
+
+        	if(N1->next == NULL && N2->next != NULL){
+        		N1->val = 0;
+        		N2 = N2->next;
+        		continue;
+        	}
+
+        	if(N1->next != NULL && N2->next == NULL){
+        		N2->val = 0;
+        		N1 = N1->next;
+        		continue;
+        	}
+
+        	if(N1->next != NULL && N2->next != NULL){
+        		N1 = N1->next;
+        		N2 = N2->next;
+        		continue;
+        	}
+
+        	
+
+        } while (true);
+        	
+        	
+    }
+};
