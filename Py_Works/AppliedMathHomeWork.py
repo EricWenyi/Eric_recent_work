116e8609d0f5f579c2af82be55d2006a5151516b
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-04-26 16:33:31 +0800

    Finish RSA programming without Tkinter GUI

--- Py_Works/AppliedMathHomeWork.py
+++ Py_Works/AppliedMathHomeWork.py
@@ -8,18 +8,20 @@ def gcd(a,b):
 
 def test_of_prime(num):
     a = 2
-    flag = False
+    flag = True
     while(a<=(num ** 0.5) and a % 2 != 0):
         if(num % a == 0):
-            flag = True
+            flag = False
+
+        a += 1
 
     return flag
 
 def multiplicate_inverse(e,phi):
     d = 0
     x1 = 0
-    x2 = 0
-    y1 = 0
+    x2 = 1
+    y1 = 1
     temp_phi = phi
 
     while e > 0:
@@ -28,7 +30,7 @@ def multiplicate_inverse(e,phi):
         temp_phi = e
         e = temp2
 
-        x = x2 - temp1 * x1
+        x = x2- temp1* x1
         y = d - temp1 * y1
 
         x2 = x1
@@ -63,6 +65,41 @@ def generate_key(p,q):
     while g != 1:
         e = random.randrange(1,phi)
         g = gcd(e,phi)
+
+    #use extended Euclid's Algorithm to generate the key
+    d = multiplicate_inverse(e,phi)
+
+
+    return ((e,n),(d,n))
+
+def encrypt(pk,message):
+    public_key ,n = pk
+    #generate a encryted message C in the form of a ^ b % c
+    c = (message ** public_key) % n
+    return c
+
+def discrypt(sk,c):
+    private_key,n = sk
+
+    message = (c ** private_key) % n
+    return message
+
+print "RSA Encrypter/ Decrypter"
+p = int(raw_input("Enter a prime number (17, 19, 23, etc): "))
+q = int(raw_input("Enter another prime number (Not one you entered above): "))
+print "Generating your public/private keypairs now . . ."
+private, public = generate_key(p, q)
+print "Your public key is ", public ," and your private key is ", private
+message = int(raw_input("Enter a message to encrypt with your public key: "))
+encrypted_msg = encrypt(public, message)
+print "Your encrypted message is: ",encrypted_msg
+print "Decrypting message with private key ", private ," . . ."
+original_msg = discrypt(private,encrypted_msg)
+print "Your original message is ",original_msg
+print "Thank you for watching"
+
+
+
 
 
 
