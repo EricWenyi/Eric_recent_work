38f1530d502b558b536a6b13f164056a840f7fdf
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-07-15 23:14:11 +0800

    Leetcode

---
+++ C_Workspace/atoi/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string str) {
+
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ C_Workspace/playground/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char a[256];
+    printf("Enter the number you want: ");
+    fgets(a,256,stdin);
+    int i;
+    i = atoi(a);
+    cout<<i;
+}
