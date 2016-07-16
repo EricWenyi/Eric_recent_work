c6c7020e71c9fa792e8f273452f8c24fc8a44827
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-07-16 15:08:26 +0800

    leetcode

--- C_Workspace/atoi/main.cpp
+++ C_Workspace/atoi/main.cpp
@@ -6,12 +6,65 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
+        if(str.size() == 0)
+            return 0;
 
+        int i = 0;//wipe white space ahead of the sequence
+
+        while(str[i] == ' '){
+            i++;
+        }
+
+        int num = 0;
+        bool flag = true;
+        bool overflow = false;
+        int  oldnum = 0;
+
+        if(str[i] == '+')
+            i++;
+        else if(str[i] == '-'){
+            flag = false;
+            i++;
+        }
+        else if(str[i] >'9' || str[i] < '0'){
+            return 0;
+        }
+
+        while(str[i] <= '9' && str[i] >= '0'){
+            oldnum = num;
+            num = num*10 + str[i] - '0';
+
+            if(num/10 != oldnum){
+                overflow = true;
+            }
+
+            i++;
+        }
+//        cout<<overflow<<endl;
+//        cout<<oldnum<<endl;
+//        cout<<num<<endl;
+
+        if(overflow){
+            if(flag){
+                return INT_MAX;
+            }
+            else
+                return INT_MIN;
+        }
+
+        if(!flag){
+            num = -num;
+        }
+
+        return num;
     }
 };
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Solution s;
+    int num;
+    string a = "    10522545459";
+    num = s.myAtoi(a);
+    cout<<num<<endl;
 }
