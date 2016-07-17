7d4368da3c04eaa7aeecc0c0163cf13f0a006c26
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-07-17 16:06:26 +0800

    Yeah! Finish ZigZag

--- C_Workspace/zigzag/main.cpp
+++ C_Workspace/zigzag/main.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
     string convert(string s, int numRows) {
+            if(numRows == 0 || numRows == 1){
+                return s;
+            }
+            if(s.empty()){
+                return s;
+            }
+
             int pattern = numRows*2 - 2;
             int period = s.size() / pattern;
             int remainder = s.size() % pattern;
 
+            //cout<<pattern<<" "<<period<<" "<<remainder<<endl;;
+
             int Lines = period * (numRows - 1);
 
-            if(remainder <= numRows){
+            //cout<<Lines<<endl;
+
+            if(remainder <= numRows && remainder>0){
                 Lines++;
             }
-            else{
+            else if (remainder > numRows && remainder < pattern){
                 Lines += Lines + (remainder - numRows + 1);
             }
 
-            char zigzag[numRows][Lines] = {{'0'}};
+            //cout<<Lines<<endl;
+
+            char zigzag[numRows][Lines];
+
+            for(int i = 0; i < numRows; i++){
+                for(int j = 0; j < Lines; j++){
+                    zigzag[i][j] = '0';
+                }
+            }
+
+
 
             bool flag = false;//a flag showing whether we need to reverse the string
 
             int i = 0;
             int j = 0;
 
-            for(int k = 0; k < s.size(); k++){
+            for(size_t k = 0; k < s.size(); k++){
                 if(!flag){
                     zigzag[i][j] = s[k];
 
@@ -42,7 +64,7 @@ public:
 
                     if(i == 0){
                         flag = false;
-                        j++;
+                        i++;
                     }
                     else{
                         i--;
@@ -51,21 +73,36 @@ public:
                 }
             }
 
+            char * new_s = new char[s.size()];
+
+//            for(int i = 0; i < numRows; i++){
+//                for(int j = 0; j < Lines; j++){
+//                   cout<<zigzag[i][j]<<" ";
+//                }
+//                cout<<endl;
+//            }
+            int k = 0;
             for(int i = 0; i < numRows; i++){
                 for(int j = 0; j < Lines; j++){
-                    cout<<zigzag[i][j]<<" ";
+                    if(zigzag[i][j] != '0'){
+                        new_s[k] = zigzag[i][j];
+                        k++;
+                    }
                 }
-                cout<<endl;
             }
+            //cout<<new_s[0]<<endl;
             //To be modified
-            return s;
+            string b(new_s,s.size());
+            return b;
     }
 };
 
 int main()
 {
-    string s = "ASPISNET";
+    string s = "A";
+    string b;
     Solution solu;
-    solu.convert(s,3);
+    b = solu.convert(s,1);
+    cout<<b;
     return 0;
 }
