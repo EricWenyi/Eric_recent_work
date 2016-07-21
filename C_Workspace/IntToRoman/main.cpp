7b3e9b69ff743f26f1f7ad205429e88e67b819ff
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-07-21 22:59:30 +0800

    IntToRoman

--- C_Workspace/IntToRoman/main.cpp
+++ C_Workspace/IntToRoman/main.cpp
@@ -20,12 +20,73 @@ public:
         }
         string s = "";
 
+        while(!bits.empty()){
+            int bit = bits.top();
 
+            if(bits.size() == 4){
+                if(bit == 3){
+                    s.append("MMM");
+                }
+                else if(bit == 2){
+                    s.append("MM");
+                }
+                else if(bit == 1){
+                    s.append("M");
+                }
+            }
+
+            if(bits.size() == 3){
+                switch(bit){
+                    case 1: s.append("C");break;
+                    case 2: s.append("CC"); break;
+                    case 3: s.append("CCC"); break;
+                    case 4: s.append("DC"); break;
+                    case 5: s.append("D"); break;
+                    case 6: s.append("DC"); break;
+                    case 7: s.append("DCC"); break;
+                    case 8: s.append("DCCC"); break;
+                    case 9: s.append("CM"); break;
+                }
+            }
+
+            if(bits.size() == 2){
+                switch(bit){
+                    case 1: s.append("X"); break;
+                    case 2: s.append("XX"); break;
+                    case 3: s.append("XXX"); break;
+                    case 4: s.append("LX"); break;
+                    case 5: s.append("L"); break;
+                    case 6: s.append("LX"); break;
+                    case 7: s.append("LXX"); break;
+                    case 8: s.append("LXXX"); break;
+                    case 9: s.append("XC"); break;
+                }
+            }
+
+            if(bits.size() == 1){
+                switch(bit){
+                    case 1: s.append("I"); break;
+                    case 2: s.append("II"); break;
+                    case 3: s.append("III"); break;
+                    case 4: s.append("IV"); break;
+                    case 5: s.append("V"); break;
+                    case 6: s.append("VI"); break;
+                    case 7: s.append("VII"); break;
+                    case 8: s.append("VIII"); break;
+                    case 9: s.append("IX"); break;
+                }
+            }
+
+            bits.pop();
+        }
+
+        return s;
     }
 };
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Solution s;
+    string sl = s.intToRoman(3999);
+    cout<<sl<<endl;
 }
