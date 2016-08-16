289191a3df8e53a970d57ddfcc851194cfc39641
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-08-16 11:41:06 -0700

    I am back

--- C_Workspace/removeElement/main.cpp
+++ C_Workspace/removeElement/main.cpp
@@ -5,34 +5,41 @@ using namespace std;
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+
         if(nums.size() == 0){
             return 0;
         }
+
         int p1,p2;
+        int counter = 0;
 
         p1 = 0;
         p2 = nums.size() - 1;
 
-        while(p1 <= p2){
+        while(p1 <= p2 ){
 
-            while(nums[p2] == val){
+            while(nums[p2] == val && p2 >= 0){
                 p2--;
             }
 
             if(nums[p1] == val){
+
                 int temp;
                 temp = nums[p1];
                 nums[p1] = nums[p2];
                 nums[p2] = temp;
+                counter++;
+
                 p1++;
                 p2--;
             }
             else{
                 p1++;
+                counter++;
             }
         }
 
-        return p1+1 ;
+        return counter;
     }
 };
 
@@ -40,12 +47,12 @@ int main()
 {
     vector<int> nums;
 
-    for(int i = 0; i < 5; i++){
-        nums.push_back(i);
+    for(int i = 0; i < 2; i++){
+        nums.push_back(3);
     }
 
     Solution s;
-    int n = s.removeElement(nums,3);
+    int n = s.removeElement(nums,4);
     for(size_t i = 0; i < nums.size(); i++){
         cout<<nums[i]<<endl;
     }
