5387b701859bc103b5f9b197cdb40eb4335a6722
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-08-30 11:09:42 -0700

    StartedWorking

---
+++ C_Workspace/CompareVersionNumber/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/>
+using namespace std;
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
