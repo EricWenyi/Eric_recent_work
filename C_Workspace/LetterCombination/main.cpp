073a042424e4d7728e4a8358174797c8f0c03f43
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-09-10 20:13:32 -0700

    Finished LetterCombinatino

---
+++ C_Workspace/LetterCombination/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+
+        string digitalmap[] = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        res.push_back("");
+
+        for(int i = 0; i < digits.size(); i++){
+            vector<string> temp;
+            for(int j = 0; j < res.size(); j++){
+                for(int p = 0; p < digitalmap[digits[i] - '0'].size(); p++)
+                    temp.push_back(res[j] + digitalmap[digits[i] - '0'][p]);
+            }
+
+            res = temp;
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<string> letters = s.letterCombinations("23");
+
+    for(size_t i = 0; i < letters.size(); i++){
+        cout<<letters[i]<<" ";
+    }
+
+}
