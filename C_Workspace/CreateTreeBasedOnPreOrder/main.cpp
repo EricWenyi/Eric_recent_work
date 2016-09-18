63466c8fc58883e176ee453d8912ea460d2b85c8
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-09-17 22:42:22 -0700

    Yeah just to make sure you could understand

--- C_Workspace/CreateTreeBasedOnPreOrder/main.cpp
+++ C_Workspace/CreateTreeBasedOnPreOrder/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
+
+
+ Definition of TreeNode:
+ class TreeNode {
+ public:
+     int val;
+     TreeNode *left, *right;
+     TreeNode(int val) {
+         this->val = val;
+         this->left = this->right = NULL;
+     }
+}
+
+
+
+class Solution {
+    /**
+     *@param preorder : A list of integers that preorder traversal of a tree
+     *@param inorder : A list of integers that inorder traversal of a tree
+     *@return : Root of a tree
+     */
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        ss
+    }
+};
 
 int main()
 {
