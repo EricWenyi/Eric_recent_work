8875f0bdba54e60d0fb26db196cf6fa39b3b604b
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-09-19 22:13:50 -0700

    Some more changed

---
+++ C_Workspace/PrintNumberByRecursion/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param n: An integer.
+     * return : An array storing 1 to the largest number with n digits.
+     */
+    vector<int> numbersByRecursion(int n) {
+        // write your code here
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
