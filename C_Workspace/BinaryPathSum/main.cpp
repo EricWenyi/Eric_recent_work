5035bbd13b1269a124d9fac27334b2f1aa36ba69
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-09-25 19:31:08 -0700

    YEah just in case

--- C_Workspace/BinaryPathSum/main.cpp
+++ C_Workspace/BinaryPathSum/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+
+
+ class TreeNode {
+ public:
+     int val;
+     TreeNode *left, *right;
+     TreeNode(int val) {
+         this->val = val;
+         this->left = this->right = NULL;
+     }
+ };
+
+class Solution {
+public:
+    /**
+     * @param root the root of binary tree
+     * @param target an integer
+     * @return all valid paths
+     */
+    vector<vector<int> > binaryTreePathSum(TreeNode *root, int target) {
+        // Write your code here
+        vector<vector<int> > result;
+
+
+
+
+    }
+};
 
 int main()
 {
