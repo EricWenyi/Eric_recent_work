a7d226c0429c0abe53eaeb5199925eaee5d7b1da
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-04 17:09:00 -0700

    Yeah just started another one

---
+++ C_Workspace/copyRandomList/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ Definition for singly-linked list with a random pointer.
+ struct RandomListNode {
+     int label;
+     RandomListNode *next, *random;
+     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ };
+
+class Solution {
+public:
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        // write your code here
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
