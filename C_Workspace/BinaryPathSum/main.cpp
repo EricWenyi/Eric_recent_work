529d1e027009c46f623898a2d6b87564a1ddf08b
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-06 16:35:28 -0700

    Finally finished one procesure

--- C_Workspace/BinaryPathSum/main.cpp
+++ C_Workspace/BinaryPathSum/main.cpp
@@ -24,16 +24,65 @@ public:
      */
     vector<vector<int> > binaryTreePathSum(TreeNode *root, int target) {
         // Write your code here
-        vector<vector<int> > result;
 
+        vector<vector<int> > results;
+        vector<TreeNode*> paths;
 
+        if(root == NULL)
+            return results;
 
+        RecPath(root,target,results,paths);
+        return results;
 
+    }
+
+    void RecPath(TreeNode* root, int const target, vector<vector<int> > &results, vector<TreeNode*> paths){
+        if(root == NULL)
+            return;
+        if(root->left != NULL || root->right != NULL){
+            paths.push_back(root);
+            RecPath(root->left, target, results, paths);
+            RecPath(root->right, target, results, paths);
+        }
+        else{//is leaf
+            paths.push_back(root);
+            int temp = 0;
+            for(size_t i = 0; i < paths.size(); i++){
+                temp += paths[i]->val;
+            }
+            if(temp == target){
+                vector<int> path_val;
+                for(size_t i = 0; i < paths.size(); i++){
+                    path_val.push_back(paths[i]->val);
+                }
+                results.push_back(path_val);
+                paths.pop_back();
+            }
+            else{
+                paths.pop_back();
+            }
+        }
+        return;
     }
 };
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    TreeNode *root = new TreeNode(3);
+    TreeNode *left = new TreeNode(2);
+    TreeNode *right = new TreeNode(2);
+
+    root->left = left;
+    root->right = right;
+
+    Solution s;
+    vector<vector<int> > results;
+    results = s.binaryTreePathSum(root,5);
+    for(size_t i =0; i < results.size(); i++){
+        for(size_t j = 0; j < results[i].size(); j++){
+            cout<<results[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 }
