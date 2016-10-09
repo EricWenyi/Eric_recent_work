97602c8fc28c8a478d6462cad594dde5b62e5072
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-08 22:52:31 -0700

    Yeah

--- C_Workspace/IntersectionLinkedList/main.cpp
+++ C_Workspace/IntersectionLinkedList/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
-#include <bits/std>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    /**
+     * @param headA: the first list
+     * @param headB: the second list
+     * @return: a ListNode
+     */
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // write your code here
+        stack<ListNode*> A;
+        stack<ListNode*> B;
+        ListNode* result = NULL;
+
+        while(headA != NULL){
+            A.push(headA);
+            headA = headA->next;
+        }
+
+        while(headB != NULL){
+            B.push(headB);
+            headB = headB->next;
+        }
+
+        while(A.top() == B.top()){
+            result = A.top();
+            A.pop();
+            B.pop();
+        }
+
+        return result;
+    }
+};
 
 int main()
 {
