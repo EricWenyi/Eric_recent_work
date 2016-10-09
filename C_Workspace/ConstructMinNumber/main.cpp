3735ef84c5a913cad7ab36dd7f05b72afa2326c6
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-09 01:09:26 -0700

    Keep moving!

---
+++ C_Workspace/ConstructMinNumber/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param nums n non-negative integer array
+     * @return a string
+     */
+    string minNumber(vector<int>& nums) {
+        // Write your code here
+        vector<string> luyun;
+        string results;
+
+        for(size_t i = 0; i < nums.size(); i++){
+            string temp;
+            itoa(nums[i], temp, 10);
+        }
+
+        for(size_t i = 0; i < nums.size(); i++){
+            for(size_t )
+        }
+
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
