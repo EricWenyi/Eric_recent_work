680addc788e02d9659682b12b53cdebd7d534523
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-11 23:31:11 -0700

    Just Do It!

--- C_Workspace/ReversePairs/main.cpp
+++ C_Workspace/ReversePairs/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<bits/stdc++.h>
-
+using namespace std;
 class Solution {
 public:
     /**
@@ -9,7 +9,29 @@ public:
      */
     long long reversePairs(vector<int>& A) {
         // Write your code here
+        vector<int> temp;
+        long long results = 0;
+        int length = A.size();
+        temp.push_back(A[length - 1]);
 
+        for(int i = A.size() - 2; i >= 0; i--){//binary search
+            int left = 0;
+            int right = temp.size();
+            while(left < right){
+                int mid = (left + right) / 2;
+                if(A[i] > temp[mid]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid;
+                }
+            }
+
+            temp.insert(temp.begin() + right, A[i]);
+            results += right;
+        }
+
+        return results;
     }
 };
 
@@ -17,6 +39,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> i;
+    i.push_back(5);
+    i.push_back(3);
+
+    Solution s;
+    long long res =  s.reversePairs(i);
+
+    cout<<res;
+
     return 0;
 }
