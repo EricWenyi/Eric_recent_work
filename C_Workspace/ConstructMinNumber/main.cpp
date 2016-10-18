38cf17eb0063364c371f29944bf998cff10d2ad3
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-18 10:23:58 -0700

    BoysAndGirls I am back

--- C_Workspace/ConstructMinNumber/main.cpp
+++ C_Workspace/ConstructMinNumber/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
+
+bool compare(string A, string B){
+        return ((A+B) < (B + A));
+}
 
 class Solution {
 public:
@@ -9,13 +14,38 @@ public:
      * @return a string
      */
 
-    bool compare(string A, string B){
-        return ((A+B) > (B + A));
-    }
+
+
     string minNumber(vector<int>& nums) {
         // Write your code here
+        vector<string> s(nums.size());
+        stringstream   stream;
+        for(int i = 0; i < nums.size(); i++){
+            stream << nums[i];
+            stream >> s[i];
+            stream.clear();
+        }
 
+        sort(s.begin(), s.end(), compare);
 
+        string result = "";
+        for(size_t i = 0; i < s.size(); i++){
+            result += s[i];
+        }
+
+        string res;
+        bool flag = false;
+        for (int i = 0; i < result.size(); ++i) {
+            if (result[i] != '0') {
+                res.push_back(result[i]);
+                flag = true;
+            } else if (flag) {
+                res.push_back(result[i]);
+            }
+        }
+        if (!flag) res.push_back('0');
+
+        return res;
     }
 
 
@@ -23,6 +53,14 @@ public:
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    string a;
+    Solution s;
+    int b[] = {0,0,55,11};
+    vector<int> nums(b, b + sizeof(b) / sizeof(int));
+
+//    for(int i = 0; i < nums.size(); i++){
+//        cout<<nums[i]<<endl;
+//    }
+    a = s.minNumber(nums);
+    cout<<a<<endl;
 }
