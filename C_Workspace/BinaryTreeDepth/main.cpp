7a2913987fbcc60f34fd5dc5fa66619f05107d6c
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-28 10:26:25 +0800

    Yeah

--- C_Workspace/BinaryTreeDepth/main.cpp
+++ C_Workspace/BinaryTreeDepth/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+
+    int maxDepth(TreeNode *root) {
+        // write your code here
+        if(root == NULL)
+            return 0;
+
+        int n_left = maxDepth(root->left);
+        int n_right = maxDepth(root->right);
+
+        return (n_left>n_right) ? (n_left + 1) : (n_right + 1);
+    }
+};
 
 int main()
 {
---
+++ C_Workspace/SingleNumber/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<bit>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
