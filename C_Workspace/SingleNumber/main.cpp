c6aaae56409f4ff5e653039e4bd7eb3b357b7ba4
Eric_Wenyi <ericwenyi1995@outlook.com>
2016-10-28 10:35:34 +0800

    WWWWWWWWWWWWWWWWHa

--- C_Workspace/SingleNumber/main.cpp
+++ C_Workspace/SingleNumber/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include<bit>
+#include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+	/**
+	 * @param A: Array of integers.
+	 * return: The single number.
+	 */
+    int singleNumber(vector<int> &A) {
+        // write your code here
+        map<int,int> dic;
+        for(int i = 0 ; i < A.size(); i++){
+            dic[A[i]]++;
+        }
+
+        map<int,int>::iterator pos = dic.begin();
+
+        for(;pos!=dic.end(); pos++){
+            if(pos->second == 1)
+                return pos->first;
+        }
+    }
+};
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int a[] = {2,4,4};
+    vector<int> A(a,a + sizeof(a) / sizeof(int));
+
+    Solution s;
+    cout<<s.singleNumber(A);
 }
