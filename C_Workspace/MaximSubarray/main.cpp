3eedf8bc88aad5ea0020e45ab601ef43178344c9
Eric_Wenyi <ericwenyi1995@outlook.com>
2017-02-23 21:39:48 +0800

    Well

--- C_Workspace/MaximSubarray/main.cpp
+++ C_Workspace/MaximSubarray/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int n = nums.size();
+            int dp[n] = {0};
+            int max = nums[0];
+
+            dp[0] = nums[0];
+
+            for(int i = 1; i < n; i++){
+                dp[i] = nums[i] + (dp[i -1] > 0 ? dp[i - 1] : 0);
+
+                if(dp[i] > max)
+                    max = dp[i]
+            }
+
+            return max;
+        }
+};
