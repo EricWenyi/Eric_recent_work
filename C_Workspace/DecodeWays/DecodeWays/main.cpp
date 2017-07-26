5c4ee7b9fc8105992512ab592d13a052bc7fba72
Eric_Wenyi <ericwenyi1995@outlook.com>
2017-07-27 00:19:30 +0800

    The solving method is not good

---
+++ C_Workspace/DecodeWays/DecodeWays/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  DecodeWays
+//
+//  Created by 邬文弈 on 2017/7/26.
+//  Copyright © 2017年 Eric. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param s a string,  encoded message
+     * @return an integer, the number of ways decoding
+     */
+    int numDecodings(string& s) {
+        // Write your code here
+        
+        
+    }
+};
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
