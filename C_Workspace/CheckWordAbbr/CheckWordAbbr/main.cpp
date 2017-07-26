170d94ee2f8afa92a9dec4c2049a9b73aeee3737
Eric_Wenyi <ericwenyi1995@outlook.com>
2017-07-26 22:54:37 +0800

    another one

--- C_Workspace/CheckWordAbbr/CheckWordAbbr/main.cpp
+++ C_Workspace/CheckWordAbbr/CheckWordAbbr/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    /**
+     * @param word a non-empty string
+     * @param abbr an abbreviation
+     * @return true if string matches with the given abbr or false
+     */
+    bool validWordAbbreviation(string& word, string& abbr) {
+        // Write your code here
+        int length = 0;
+        int lenOfNum = 0;
+        for(int i = 0, j = 0; i < abbr.size() && j < word.size(); i++, j++){
+            if(word[i] != abbr[j]){
+                if(abbr[j]>48 && abbr[j] < 58){
+                    
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+
+    
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Solution s;
+    
+    
+    
+    string word = "iauiuiauruwaioushfjsahfjahwuewhdjahsjdhjwaeiuwjdajskdjshajeuiwdsjkahjdhasjdhkjw";
+    string abbr = "iauiuiauruwaioushfj13jahsjdhjwaeiuwjd1js1d1shajeuiwdsjkahjdhasjdhkjw";
+    
+    cout<<s.validWordAbbreviation(word, abbr)<<endl;;
+    
+    
 }
---
+++ C_Workspace/TriangleOverlap/TriangleOverlap/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  TriangleOverlap
+//
+//  Created by 邬文弈 on 2017/7/25.
+//  Copyright © 2017年 Eric. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+struct Point {
+    int x;
+    int y;
+    Point() : x(0), y(0) {}
+    Point(int a, int b) : x(a), y(b) {}
+};
+
+class Solution {
+public:
+    /**
+     * @param l1 top-left coordinate of first rectangle
+     * @param r1 bottom-right coordinate of first rectangle
+     * @param l2 top-left coordinate of second rectangle
+     * @param r2 bottom-right coordinate of second rectangle
+     * @return true if they are overlap or false
+     */
+    bool doOverlap(Point& l1, Point& r1, Point& l2, Point& r2) {
+        // Write your code here
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
