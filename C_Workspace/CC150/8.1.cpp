49c31faded69f4317a3380fcfa5889b9cad9f975
Eric_Wenyi <ericwenyi1995@outlook.com>
2017-10-02 16:24:44 -0700

    new one

--- C_Workspace/CC150/8.1.cpp
+++ C_Workspace/CC150/8.1.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 
-using namespace std
+using namespace std;
+
+int WaysToSteps(int n){
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+	else if(n == 2) return 2;
+	else if(n == 3) return 4;
+
+	int * a = new int[n+1];
+	a[0] = 0;
+	a[1] = 1;
+	a[2] = 2;
+	a[3] = 4;
+
+	for(int i = 4; i < n+1; i++){
+		a[i] = a[i-1] + a[i-2] + a[i-3]; 
+	}
+	return a[n];
+
+}
 
 int main(){
-    
+    int res = WaysToSteps(5);
+    cout<<res<<endl;
+
+    return 0;
 }
